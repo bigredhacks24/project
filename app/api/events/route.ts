cd327fd9fbe36e38879c5fbc5f1d954a6197a817
Temi <ta375@cornell.edu>
2024-10-05 14:30:28 -0400

    Working CRUD for groups and friends

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,29 +1,33 @@
-import { createClient } from "@/utils/supabase/server";
-import { NextResponse } from "next/server";
+// import { createClient } from "@/utils/supabase/server";
+// import { NextResponse } from "next/server";
 
-// Fetch all events for a specific user
-export async function GET(req: Request) {
-  const supabase = createClient();
-  const url = new URL(req.url);
-  const personId = url.searchParams.get("personId");
+// // Fetch all events for a specific user
+// export async function GET(req: Request) {
+//   const supabase = createClient();
+//   const url = new URL(req.url);
+//   const personId = url.searchParams.get("personId");
 
-  // Fetch events for a user by checking the 'event_person_attendance' table
-  const { data: events, error } = await supabase
-    .from("event")
-    .select(
-      `
-      *,
-      event_person_attendance (
-        attending
-      )
-    `
-    )
-    .eq("event_person_attendance.person_id", personId);
+//   if (!personId) {
+//     return NextResponse.json({ error: "personId is required" }, { status: 400 });
+//   }
 
-  if (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
-  }
+//   // Fetch events for a user by checking the 'event_person_attendance' table
+//   const { data: events, error } = await supabase
+//     .from("event")
+//     .select(
+//       `
+//       *,
+//       event_person_attendance (
+//         attending
+//       )
+//     `
+//     )
+//     .eq("event_person_attendance.person_id", personId);
 
-  // Return the full event data where the user is attending
-  return NextResponse.json(events, { status: 200 });
-}
+//   if (error) {
+//     return NextResponse.json({ error: error.message }, { status: 500 });
+//   }
+
+//   // Return the full event data where the user is attending
+//   return NextResponse.json(events, { status: 200 });
+// }

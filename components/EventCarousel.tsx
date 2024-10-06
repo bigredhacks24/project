a905255dd5052b017ea1e3dd304a0278acab5f0b
Simon Ilincev <trilogicworlds@gmail.com>
2024-10-06 05:22:13 -0400

    scrolly scroll go brrr

--- components/EventCarousel.tsx
+++ components/EventCarousel.tsx
@@ -10,23 +10,11 @@ interface EventCarouselProps {
 }
 
 const EventCarousel: React.FC<EventCarouselProps> = ({ events, onEventClick }) => {
-  const [currentPage, setCurrentPage] = useState(0);
-  const eventsPerPage = 5;
-
-  const totalPages = Math.ceil(events.length / eventsPerPage);
-  const startIndex = currentPage * eventsPerPage;
-  const endIndex = startIndex + eventsPerPage;
-  const currentEvents = events.slice(startIndex, endIndex);
-
-  const nextPage = () => {
-    setCurrentPage((prev) => (prev + 1) % totalPages);
-  };
-
   return (
     <div className={`relative grid items-start gap-3 self-stretch`}>
-      <div className="grid grid-cols-5 items-start gap-3">
-        {currentEvents.map((event: EventWithAttendance) => (
-          <div key={event.event_id} onClick={() => onEventClick(event)}>
+      <div className="flex flex-row items-start gap-3 overflow-x-scroll">
+        {events.map((event: EventWithAttendance) => (
+          <div key={event.event_id} onClick={() => onEventClick(event)} className="flex-shrink-0 w-[240px]">
             <EventCard eventWithAttendance={event} />
           </div>
         ))}
